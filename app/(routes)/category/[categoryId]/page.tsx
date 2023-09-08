1553c5503f26ef0f18c9255b2c5422057e3abe47
solo-mizan <mizanbd.eee@gmail.com>
2023-09-09 03:26:19 +0600

    'MobileFilter' error fixed

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -41,7 +41,9 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                 />
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-                        <MobileFilter />
+                        <MobileFilter
+                            sizes={sizes}
+                            colors={colors} />
                         <div className="hidden lg:block">
                             <Filter
                                 valueKey="sizeId"
